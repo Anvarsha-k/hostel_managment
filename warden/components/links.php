b62d9fd35fd552ca10f703569a9f49fbea895fbe
amir <tpamir71@gmail.com>
2022-11-02 20:29:01 +0530

    componensts in warden and student

---
+++ warden/components/links.php
@@ -0,0 +1,42 @@
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+<meta name="description" content="Robust admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+<meta name="keywords" content="admin template, robust admin template, dashboard template, flat admin template, responsive admin template, web app">
+<meta name="author" content="PIXINVENT">
+<title>Everything.ae</title>
+<link rel="shortcut icon" type="image/png" href="assets/images/ico/favicon-32.png">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-touch-fullscreen" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<!-- BEGIN VENDOR CSS-->
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+<!-- font icons-->
+<link rel="stylesheet" type="text/css" href="assets/fonts/icomoon.css"> 
+<link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome.min.css"> 
+<link rel="stylesheet" type="text/css" href="assets/fonts/flag-icon-css/css/flag-icon.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/pace.css">
+<!-- END VENDOR CSS-->
+<!-- BEGIN ROBUST CSS-->
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-extended.css">
+<link rel="stylesheet" type="text/css" href="assets/css/app.css">
+<link rel="stylesheet" type="text/css" href="assets/css/colors.css">
+<link rel="stylesheet" type="text/css" href="assets/css/summernote.css">
+<link rel="stylesheet" type="text/css" href="assets/css/select2.min.css">
+<!-- END ROBUST CSS-->
+<!-- BEGIN Page Level CSS-->
+<link rel="stylesheet" type="text/css" href="assets/css/vertical-menu.css">
+<link rel="stylesheet" type="text/css" href="assets/css/vertical-overlay-menu.css">
+<link rel="stylesheet" type="text/css" href="assets/css/datatables.bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-switch.css">
+<!-- END Page Level CSS-->
+<!-- BEGIN Custom CSS-->
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/bootstrap-datepicker3.min.css" crossorigin="anonymous">
+<link href="assets/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+<link href="assets/css/mdtimepicker.css" rel="stylesheet" type="text/css">
+
+
+<!-- END Custom CSS-->
